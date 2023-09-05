c4c4de50855ebf0147b73cecfcb26dbfb482ba79
TBoltsp <sylvain.paulet01@gmail.com>
2023-09-05 17:14:37 +0200

    chore(architecture): navigation added for Educator
    add route in app.js
    add in header.jsx (#temp)
    add template un Educator.jsx

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Educator = () => {
 
     return (
         <div>
-            
+            {/* START acces for feature test */}
+            <h1>page enseignant</h1>
+            {/* <Link to="/faturePage">lien vers la page</Link> */}
+
+            {/* END acces for feature test */}
         </div>    
     );
 };

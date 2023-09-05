7184766692039127ec3b25c1f72d64a1002ba136
Louis Richards <llouis.richards@gmail.com>
2023-09-05 16:12:46 +0200

    Moved login placement

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -1,78 +1,76 @@
-import './LoginForm.css'
-import React, { Component } from 'react';
-import axios from 'axios';
+import "./LoginForm.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-const apiUrl = `http://localhost:8080`;
+const apiUrl = `http://localhost:9000`;
 
 class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
 
-    handleEmailChange(event) {
-        this.setState({ email: event.target.value });
-    }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
 
-    handlePasswordChange(event) {
-        this.setState({ password: event.target.value });
-    }
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
 
-    async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault(); 
-        const email = this.state.email;
-        const password = this.state.password;
+  async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const email = this.state.email;
+    const password = this.state.password;
 
-        const res = await axios.get(apiUrl + '/login', { email, password });
-        alert('Found user !' + res.data.user);
+    const res = await axios.get(apiUrl + "/login", { email, password });
+    alert("Found user !" + res.data.user);
 
-        // store res in localstorage
-        localStorage.setItem('token', res.data.token);
-        localStorage.setItem('user', JSON.stringify(res.data.user));
-    }
+    // store res in localstorage
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+  }
 
-    render() {
-        return (
-            <div className="login-container">
-                <img className="image" src="./laptop-new.jpg" alt="Hands on laptop" />
-                <p>Les champs marqués d'une astérisque (*) sont obligatoires</p>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="email">E-mail</label>
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleEmailChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Mot de passe</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handlePasswordChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <input className='btn' type="submit" value="Se connecter" />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="login-container">
+        <img className="image" src="./laptop-new.jpg" alt="Hands on laptop" />
+        <p>Les champs marqués d'une astérisque (*) sont obligatoires</p>
+        <form onSubmit={this.handleSubmit} className="login_form">
+          <div className="login_inputs">
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+              placeholder="Adresse e-mail*"
+              required
+              className="login_input"
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+              placeholder="Mot de passe*"
+              required
+              className="login_input"
+            />
+          </div>
+          <input className="btn" type="submit" value="Se connecter" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
--- src/pages/connection/Connection.jsx
+++ src/pages/connection/Connection.jsx
@@ -4,14 +4,17 @@ import Navbreadcrum from "../../components/navbreadcrum/Navbreadcrum";
 import LoginForm from "../../components/loginform/LoginForm";
 
 const Connection = () => {
-
-    return (
-        <div>
-            <Navbreadcrum text="Authentification à l'Espace Numérique de Formation" />
-            <IconTitle iconpath="./power.png" alt="Power Icon" title="AUTHENTIFICATION" />
-            <LoginForm />
-        </div>    
-    );
+  return (
+    <div>
+      <Navbreadcrum text="Authentification à l'Espace Numérique de Formation" />
+      <IconTitle
+        iconpath="./power.png"
+        alt="Power Icon"
+        title="AUTHENTIFICATION"
+      />
+      <LoginForm />
+    </div>
+  );
 };
 
-export default Connection
+export default Connection;
